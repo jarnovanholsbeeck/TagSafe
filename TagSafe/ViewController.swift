35a14de0dd949c73761415c5199ec10e71008426
Jarno Van Holsbeeck <jarno.van.holsbeeck@student.ehb.be>
2019-05-20 10:53:27 +0200

    Searchbar & fonts

--- TagSafe/ViewController.swift
+++ TagSafe/ViewController.swift
@@ -11,6 +11,8 @@ import Firebase
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
     @IBOutlet weak var AddButton: UIButton!
     @IBOutlet weak var RecordAudioButton: UIButton!
     @IBOutlet weak var RecordVideoButton: UIButton!
@@ -24,6 +26,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        editSearchBar()
         
         db = Firestore.firestore()
         
@@ -44,6 +48,20 @@ class ViewController: UIViewController {
         RecordAudioButton.center = AddButton.center
         RecordVideoButton.center = AddButton.center
         TakeNoteButton.center = AddButton.center
+    }
+    
+    func editSearchBar() {
+        if let textfield = SearchBar.value(forKey: "searchField") as? UITextField {
+            textfield.backgroundColor = UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0)
+            textfield.borderStyle = .none
+            textfield.borderStyle = .roundedRect
+            textfield.font = UIFont(name: "calibretest_regular", size: 14)
+            
+            if let leftView = textfield.leftView as? UIImageView {
+                leftView.image = leftView.image?.withRenderingMode(.alwaysTemplate)
+                leftView.tintColor = UIColor(red:0.00, green:0.42, blue:1.00, alpha:1.0)
+            }
+        }
     }
     
     @IBAction func addButtonClicked(_ sender: UIButton) {
