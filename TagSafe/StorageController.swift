13c704cae0535e551b2da8cee4f17ec1d8b62ae7
Willem-Jan Pattyn <willem-jan.pattyn@student.ehb.be>
2019-05-21 11:41:24 +0200

    image picker: select image data and get filename

--- TagSafe/StorageController.swift
+++ TagSafe/StorageController.swift
@@ -7,16 +7,28 @@
 //
 
 import UIKit
+import FirebaseStorage
 
-class StorageController: UIViewController {
+class StorageController: UIViewController{
 
+    var storage: Storage?
+    var mediaURL: Any?
+    
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        storage = Storage.storage()
     }
     
-
+    @IBAction func selectImage(_ sender: Any) {
+        handleSelectImage()
+    }
+    
+    @IBAction func uploadImage(_ sender: Any) {
+        handleUploadImage()
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -27,4 +39,52 @@ class StorageController: UIViewController {
     }
     */
 
+}
+
+extension StorageController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func handleSelectImage(){
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        
+        present(imagePicker, animated: true)
+    }
+    
+    func handleUploadImage(){
+        let storageRef = storage?.reference()
+        print(storageRef!)
+        
+        let filename = ((mediaURL as! NSURL).absoluteString! as NSString).lastPathComponent
+        print(filename)
+        
+//        let localFile = URL(string: (mediaURL as? String)!)
+//
+//        let uploadTask = storageRef?.putFile(from: localFile!)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        var selectedImageFromPicker: UIImage?
+        //print(info)
+        
+        if let editedImage = info[.editedImage] as? UIImage {
+            selectedImageFromPicker = editedImage
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            selectedImageFromPicker = originalImage
+        }
+        
+        if let selectedImage = selectedImageFromPicker {
+            imageView.image = selectedImage
+            mediaURL = info[.imageURL]
+            print(mediaURL!)
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 }
